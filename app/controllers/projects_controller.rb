d8892238be7d906427c1969469b61dba9ee9da36
akiii <akiii@sfc.wide.ad.jp>
2012-07-01 17:18:39 +0900

    Moified a controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
 
   def index
     @user = current_user
-    @projects = Project.all
+    @projects = @user.projects
   end
 
   def new
@@ -20,13 +20,40 @@ class ProjectsController < ApplicationController
     end
     project.released = false
     project.save
-    Participant.create(:project_id => project.id, :user_id => current_user.id, :authority => true, :isAllowed => true)
+    Participant.create(:project_id => project.id, :user_id => current_user.id, :authority => true, :is_allowed => true)
 
     redirect_to projects_path
   end
 
   def edit
+    @project = Project.find(params[:id])
+    if current_user.can_edit(@project)
+#      @notAllowedUsers = 
+    else
+      redirect_to projects_path
+    end
+  end
 
+  def update
+    project = Project.find(params[:id])
+    if current_user.can_edit(project)
+      project.name = params[:project][:name]
+      project.description = params[:project][:description]
+      if params[:open] == "open"
+        project.open = true
+      else
+        project.open = false
+      end
+      if params[:release] == "release"
+        project.released = true
+      else
+        project.released = false
+      end
+      project.save
+      redirect_to projects_path
+    else
+      redirect_to projects_path
+    end
   end
 
 end
