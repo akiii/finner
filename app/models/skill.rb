ab2bc3407bfd211717b3d1cde9811f367fd3db37
Akifumi <Akifumi@net44-dhcp81.sfc.keio.ac.jp>
2012-07-14 04:09:14 +0900

    Added relation codes.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
 class Skill < ActiveRecord::Base
   attr_accessible :name, :skill_category_id
+  belongs_to :skill_category
 end
--- app/models/skill_category.rb
+++ app/models/skill_category.rb
@@ -1,3 +1,4 @@
 class SkillCategory < ActiveRecord::Base
   attr_accessible :name
+  has_many :skills
 end
