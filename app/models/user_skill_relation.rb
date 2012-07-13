f46f024d8e03f9ce0a02c047651a1988b8696d5e
Akifumi <Akifumi@net44-dhcp81.sfc.keio.ac.jp>
2012-07-14 04:08:46 +0900

    Added a new model.

---
+++ app/models/user_skill_relation.rb
@@ -0,0 +1,3 @@
+class UserSkillRelation < ActiveRecord::Base
+  attr_accessible :skill_id, :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120713154404) do
+ActiveRecord::Schema.define(:version => 20120713190729) do
 
   create_table "contacts", :force => true do |t|
     t.integer  "to_user_id"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(:version => 20120713154404) do
     t.integer  "department_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "user_skill_relations", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "skill_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
